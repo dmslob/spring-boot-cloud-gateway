9d177ea4c5d227d600ab921141dd1b324ba80705
Dmytro <dmytro.slobodenyuk@gmail.com>
2019-01-28 22:32:24 +0200

    spring cloud example init

---
+++ gallery-service/src/main/java/com/dmslob/galleryservice/model/Gallery.java
@@ -0,0 +1,24 @@
+package com.dmslob.galleryservice.model;
+
+import java.util.List;
+
+public class Gallery {
+    private long id;
+    private List<Object> images;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Object> getImages() {
+        return images;
+    }
+
+    public void setImages(List<Object> images) {
+        this.images = images;
+    }
+}
---
+++ image-service/src/main/java/com/dmslob/imageservice/model/Image.java
@@ -0,0 +1,37 @@
+package com.dmslob.imageservice.model;
+
+public class Image {
+    private long id;
+    private String title;
+    private String url;
+
+    public Image(long id, String title, String url) {
+        this.id = id;
+        this.title = title;
+        this.url = url;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
